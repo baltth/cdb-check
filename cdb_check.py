dcd03214344352aa2695f861d74fefc9ece7379c
baltth <baltth@gmail.com>
2025-03-25 17:46:50 +0100

    Version bumped to 0.1.0

--- cdb_check.py
+++ cdb_check.py
@@ -20,7 +20,7 @@ __author__ = "Balazs Toth"
 __email__ = "baltth@gmail.com"
 __copyright__ = "Copyright 2025, Balazs Toth"
 __license__ = "MIT"
-__version__ = "0.0.1"
+__version__ = "0.1.0"
 
 
 @dataclass
